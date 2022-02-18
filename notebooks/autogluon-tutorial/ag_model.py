d6e17cf63ff76c976abdbb29045513be72215a98
Sergey Ermolin <ec2-user@ip-172-16-59-243.us-west-2.compute.internal>
2022-02-18 00:23:02 +0000

    fixing AutoGluon DL AMI version

--- notebooks/autogluon-tutorial/ag_model.py
+++ notebooks/autogluon-tutorial/ag_model.py
@@ -7,6 +7,32 @@ from sagemaker import image_uris
 from sagemaker.serializers import CSVSerializer
 from sagemaker.deserializers import StringDeserializer
 
+REGION_MAPPING = {
+    "af-south-1": "626614931356",
+    "ap-east-1": "871362719292",
+    "ap-northeast-1": "763104351884",
+    "ap-northeast-2": "763104351884",
+    "ap-northeast-3": "364406365360",
+    "ap-south-1": "763104351884",
+    "ap-southeast-1": "763104351884",
+    "ap-southeast-2": "763104351884",
+    "ca-central-1": "763104351884",
+    "eu-central-1": "763104351884",
+    "eu-north-1": "763104351884",
+    "eu-west-1": "763104351884",
+    "eu-west-2": "763104351884",
+    "eu-west-3": "763104351884",
+    "eu-south-1": "692866216735",
+    "me-south-1": "217643126080",
+    "sa-east-1": "763104351884",
+    "us-east-1": "763104351884",
+    "us-east-2": "763104351884",
+    "us-gov-west-1": "442386744353",
+    "us-iso-east-1": "886529160074",
+    "us-west-1": "763104351884",
+    "us-west-2": "763104351884"
+}
+
 
 class AutoGluonTraining(Framework):
     def __init__(
@@ -19,14 +45,8 @@ class AutoGluonTraining(Framework):
         hyperparameters=None,
         **kwargs,
     ):
-        image_uri = image_uris.retrieve(
-            "autogluon",
-            region=region,
-            version=framework_version,
-            py_version="py37",
-            image_scope="training",
-            instance_type=instance_type,
-        )
+        account = REGION_MAPPING[region]
+        image_uri = f"{account}.dkr.ecr.{region}.amazonaws.com/autogluon-training:{framework_version}-cpu-py37-ubuntu18.04"
         super().__init__(
             entry_point,
             source_dir,
@@ -64,14 +84,8 @@ class AutoGluonInferenceModel(MXNetModel):
     def __init__(
         self, model_data, role, entry_point, region, framework_version, instance_type, **kwargs
     ):
-        image_uri = image_uris.retrieve(
-            "autogluon",
-            region=region,
-            version=framework_version,
-            py_version="py37",
-            image_scope="inference",
-            instance_type=instance_type,
-        )
+        account = REGION_MAPPING[region]
+        image_uri = f"{account}.dkr.ecr.{region}.amazonaws.com/autogluon-inference:{framework_version}-cpu-py37-ubuntu16.04"
         super().__init__(
             model_data, role, entry_point, image_uri=image_uri, framework_version="1.8.0", **kwargs
         )
